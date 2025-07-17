29da0e7178e8ddf172a2689dd66e2604f481cdba
Hossein <rahmanitkd@gmail.com>
2025-07-17 17:14:23 +0330

    config the view and ulr whit model

--- message-board/posts/views.py
+++ message-board/posts/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
 # Create your views here.
+from .models import post
+from django.views.generic import ListView
+
+# this django generic is the way to create a list view
+# def post_view(request):
+#    posts = post.objects.all()
+#    return render(request, "post_list.html", {"posts": posts})
+
+
+class post_view(ListView):
+    template_name = "post_list.html"
+    model = post
+    # this class creat a list that name is "modelname_list"
